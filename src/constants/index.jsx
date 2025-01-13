427cfc140ff79d467a4ba4b7acc0931289de9278
nsp1425 <sparamsothy1402@gmail.com>
2025-01-13 17:58:40 -0500

    Added Projects Section.

---
+++ src/constants/index.jsx
@@ -0,0 +1,41 @@
+import project1 from "../assets/project-1.png";
+
+export const projectsInfo = [
+  {
+    id: 1,
+    title: "CNN-Based Image Classification and Feature Extraction",
+    image: project1,
+    technologies:
+      " Python, PyTorch, NumPy, Scikit-learn, Matplotlib, Google Collab",
+    description:
+      "This project focuses on using Convolutional Neural Networks (CNNs) for medical image classification. A ResNet-34 model was trained on a dataset of colorectal cancer images (MUS, NORM, STR), and along with a pre-trained ShuffleNet V2 encoder, they were applied to prostate cancer and animal face datasets. The performance of these encoders was analyzed and compared using t-SNE visualizations.",
+    link: "https://github.com/bulabd/COMP432-GroupG",
+  },
+  {
+    id: 2,
+    title: "Online Grocery Store",
+    image: project1,
+    technologies: "HTML5, CSS, JavaScript, PHP, SQL",
+    description:
+      "This is an online grocery store system featuring user authentication, product catalogs, and cart management. The project included dynamic back-end functionality for product management, enabling add, edit, and delete operations using PHP and SQL. Additionally, responsive designs were created with HTML5 and CSS to enhance the user experience, while database architecture and cart functionality were optimized to deliver a seamless shopping experience.",
+    link: "https://github.com/d-realblank/Web-demo",
+  },
+  {
+    id: 3,
+    title: "DIG (Depth Illusion Generator) ",
+    image: project1,
+    technologies: "Java, SQLite, Animation Frameworks",
+    description:
+      "This is an online grocery store system featuring user authentication, product catalogs, and cart management. The project included dynamic back-end functionality for product management, enabling add, edit, and delete operations using PHP and SQL. Additionally, responsive designs were created with HTML5 and CSS to enhance the user experience, while database architecture and cart functionality were optimized to deliver a seamless shopping experience.",
+    link: "https://bitbucket.org/speedy-gonzales/speedy-gonzales/src/master/",
+  },
+  {
+    id: 4,
+    title: "TradingView Indicators and Strategies",
+    image: project1,
+    technologies: "Pine Script, TradingView.",
+    description:
+      "This is an online grocery store system featuring user authentication, product catalogs, and cart management. The project included dynamic back-end functionality for product management, enabling add, edit, and delete operations using PHP and SQL. Additionally, responsive designs were created with HTML5 and CSS to enhance the user experience, while database architecture and cart functionality were optimized to deliver a seamless shopping experience.",
+    link: "https://www.tradingview.com/u/nsp1425/",
+  },
+];
